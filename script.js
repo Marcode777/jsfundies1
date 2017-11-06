8dc919aaa073962b8e31c2fbce386ce11563ed7a
Marcode777 <marcosebello7@gmail.com>
2017-11-06 06:05:49 -0500

    update

---
+++ script.js
@@ -0,0 +1,36 @@
+document.write("this is being written from a document.write script")
+
+function verifyFunction(){
+  var start = prompt("what is the number");
+  if(start == "90210"){
+    document.getElementById("here").innerHTML = "account verified"
+  } else {
+    alert("accoun not verified");
+  }
+}
+
+
+
+function onloadFunction(){
+  alert("This is the very first function and also functions first, when the document loads!");
+  document.getElementById("here").innerHTML = "this is being written from a document.getElementById script"
+}
+
+function questionFunction(){
+
+  var question = prompt("what is your name?");
+
+  if(question == "Marco"){
+    alert("Great, you are on your way to some great JavaScript developer skills!")
+  } else if(question == "Mark"){
+    alert("Still on the way to great JavaScript developer skills!")
+  } else {
+    alert("name not recognized yet")
+  }
+
+}
+
+function keypressFunction(){
+  document.getElementById("jumbojet").style = "background-color: yellow;"
+}
+
