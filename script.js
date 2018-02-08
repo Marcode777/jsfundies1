088bf1dec0ff164ac9315c00c982b20e31f821ba
Marcode777 <marcosebello7@gmail.com>
2018-02-08 07:01:44 -0500

    update, started working on pagination

--- script.js
+++ script.js
@@ -9,6 +9,16 @@ function verifyFunction(){
   }
 }
 
+$(document).ready(function(){
+  $("#tab").pagination({
+    items: 5, 
+    contents: 'contents',
+    previous: 'Previous',
+    next: 'Next',
+    position: 'bottom',
+  })
+})
+
 
 
 // function onloadFunction(){
