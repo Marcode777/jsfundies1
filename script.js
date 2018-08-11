24b5d074ad73683f2331db23499446f5a2dcbcef
Marcode777 <marcosebello7@gmail.com>
2018-08-11 06:29:30 -0400

    update

--- script.js
+++ script.js
@@ -247,7 +247,7 @@ let newFunctionCalculator = () => {
           }
         table += '</tr>';
       }
-      document.getElementById("theTable").innerHTML = "transformed";
+      document.getElementById("theTable").innerHTML = "transformed!!";
     }
 
     // function pathDetector() {
