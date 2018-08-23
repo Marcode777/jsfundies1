7503303a88bf8c9b3b9f12d9f271f960717e567d
Marcode777 <marcosebello7@gmail.com>
2018-08-23 08:50:16 -0400

    update

--- script.js
+++ script.js
@@ -247,7 +247,7 @@ let newFunctionCalculator = () => {
           }
         table += '</tr>';
       }
-      document.getElementById("theTable").innerHTML = "Going back to New York City!";
+      document.getElementById("theTable").innerHTML = "Back in New York City!!!";
     }
 
     // function pathDetector() {
