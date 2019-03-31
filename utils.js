21635e837eaf817045526b7cbd99c7b9225fd9df
Marcode777 <marcosebello7@gmail.com>
2019-03-31 00:03:37 -0400

    added utils or pseudo utils functions

--- utils.js
+++ utils.js
@@ -1,5 +1,21 @@
-function firstUtils(){
-  console.log('firstUtils triggered!!!!!')
-}
+// function firstUtils(){
+//   console.log('firstUtils triggered!!!!!')
+// }
 
-export default firstUtils;
+// export default firstUtils;
+
+
+
+// var Utils = (function () {
+//   return {
+//     firstFunc: function() { console.log('firstFunc triggered from utils!') },
+//     secondFunc: function() { alert('secondFunc triggered from Utils!') }
+//   }
+// }());
+
+window.Utils = (function () {
+  return {
+    firstFunc: function() { console.log('firstFunc triggered from utils!') },
+    secondFunc: function() { alert('secondFunc triggered from Utils!') }
+  }
+}());
