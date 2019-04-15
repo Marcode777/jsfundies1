8747e89381bff48bbe62061f12d7bb74a38ce973
Marcode777 <marcosebello7@gmail.com>
2019-04-15 00:02:15 -0400

    update

--- script.js
+++ script.js
@@ -825,7 +825,9 @@ function loaderTimedTrigger() {
 loaderTimedTrigger();
 
 // D3.JS testing run area
-d3.selectAll('h1').style('color', 'red'); 
+d3.selectAll('h1').style('color', 'blue');
+d3.select('div.innerD3 > h2').style('color', 'red');
+d3.select('div.innerMostD3 > h3').style('color', 'orange');
 
 
 
