1ff661a7c13bad3c2829537eba64d50bf0d87c2f
Marcode777 <marcosebello7@gmail.com>
2019-08-10 02:19:13 -0400

    update

--- script.js
+++ script.js
@@ -950,7 +950,7 @@ function searchFunction() {
     txtValue = a.textContent || a.innerText;
     if(txtValue.toUpperCase().indexOf(filter) > -1){
       li[i].style.display = '';
-      document.getElementsByClassName('searchDisplayHeader')[0].innerHTML = li[i].textContent;
+      //document.getElementsByClassName('searchDisplayHeader')[0].innerHTML = li[i].textContent;
     } else {
       li[i].style.display = 'none';
       document.getElementsByClassName('searchDisplayHeader')[0].innerHTML = '';
