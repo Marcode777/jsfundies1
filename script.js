af60d6d5e2a305b9424d92861b4eccc9e3eac881
Marcode777 <marcosebello7@gmail.com>
2019-12-29 00:00:14 -0500

    update

--- script.js
+++ script.js
@@ -1035,6 +1035,6 @@ function configUp() {
   console.log('configUp triggered');
   var target = document.getElementsByClassName('arrow-config-section-image')[0];
   target.setAttribute('style', 'transform: rotate(270deg)');
-}  
+} 
 
 
