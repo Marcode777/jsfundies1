75fd2bab586b2c41d4006653a80a01d89dd9c9c4
Marco S <myaccount@Marcos-MacBook-Pro.local>
2023-06-19 06:36:52 -0400

    update

--- test.js
+++ test.js
@@ -12,8 +12,49 @@ document.addEventListener("DOMContentLoaded", function(){
         var target = document.querySelector('p.sentences');
         if(index === array.length - 1) {
             clearInterval(sentenceChanger);
+            console.log('interval complete');
         }
         console.log('array[index++] is', array[index++]);
-        target.innerHTML = array[index++]
+        // target.innerHTML = array[index++]
     }, 1300);
 });
+
+
+
+
+
+
+// var array = ['June 21 2023', 'A very special day', 'It is your birthday'];
+// let start = 0;
+// var sentenceChanger = setInterval(function() {
+//     var target = document.querySelector('p.sentences');
+//     if(index === array.length) {
+//         clearInterval(sentenceChanger);
+//         console.log('interval complete');
+//     }
+//     console.log('array[start++] is', start[index++]);
+//     target.textContent = array[start++]
+// }, 1300);
+
+
+// var array = ['June 21 2023', 'A very special day', 'It is your birthday'];
+// let start = 0;
+// var sentenceChanger = setInterval(function() {
+//     var target = document.querySelector('p.sentences');
+//     if(start === array.length) {
+//         clearInterval(sentenceChanger);
+//         console.log('interval complete');
+//     }
+//     target.textContent = array[start++]
+// }, 1300);
+
+var array = ['June 21 2023', 'A very special day', 'It is your birthday'];
+let start = 0;
+var sentenceChanger = setInterval(function() {
+    var target = document.querySelector('p.sentences');
+    if(start === array.length) {
+        clearInterval(sentenceChanger);
+        console.log('interval complete');
+    }
+    target.textContent = array[start++]
+}, 1300);
